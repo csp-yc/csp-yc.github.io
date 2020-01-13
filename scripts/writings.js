aa472430f8845e0a3c290df645c395abfd2057d5
SocialZxy <2077757721@qq.com>
2020-01-13 15:02:48 +0800

    新页面

--- scripts/writings.js
+++ scripts/writings.js
@@ -1,9 +1,9 @@
 function write_sidebar() {
     document.write('<div class="mdui-card mdui-hoverable" style="padding: 15px">\
     <div class="sidebar-title">新人资讯</div>\
-    <div class="sidebar-item"><a href="./index.html">关于本站</a></div>\
+    <div class="sidebar-item"><a href="../about/index.html">关于本站</a></div>\
     <div class="sidebar-item"><a href="javascript:;">常见技巧</a><br></div>\
-    <div class="sidebar-item"><a href="javascript:;">梗（常见错误）</a><br></div>\
+    <div class="sidebar-item"><a href="../common-mistakes/index.html">梗（常见错误）</a><br></div>\
     <div class="sidebar-item"><a href="javascript:;">学习资源</a><br></div>\
 </div>\
 <div class="mdui-card mdui-hoverable" style="padding: 15px; margin-top: 20px">\
@@ -28,8 +28,29 @@ function write_sidebar() {
     <div class="sidebar-item"><a href="javascript:;">写作须知</a></div>\
 </div>\
 <div class="mdui-card mdui-hoverable" style="padding: 15px; margin-top: 20px">\
-    <div class="sidebar-title">社区</div>\
+    <div class="sidebar-title">issues</div>\
     <div class="sidebar-item"><a href="javascript:;">BUG反馈</a></div>\
     <div class="sidebar-item"><a href="javascript:;">讨论区</a></div>\
 </div>');
+}
+
+function write_headbar() {
+    document.write('<div class="mdui-appbar mdui-appbar-fixed mdui-appbar-scroll-toolbar-hide" style="background-color: white;">\
+        <div class="mdui-toolbar mdui-color-theme">\
+            <a href="../../index.html" class="mdui-typo-title"><img src="https://s2.ax1x.com/2019/12/22/QxY8MR.png" height="25px" style="margin-bottom: -5px"> 异常OI</a>\
+            <div class="mdui-toolbar-spacer"></div>\
+            <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">search</i></a>\
+        </div>\
+        <div class="mdui-tab mdui-color-theme">\
+            <a href="../../index.html" class="mdui-ripple mdui-ripple-white mdui-tab-active">首页</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">语言基础</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">基础算法</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">动态规划</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">数据结构</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">数学</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">图论</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">计算几何</a>\
+            <a href="#" class="mdui-ripple mdui-ripple-white">杂项</a>\
+        </div>\
+    </div>');
 }
