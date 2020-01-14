aea1e0817d7a2fc1a874184ca9574bfde745dfd9
0And1Story <witherman233@gmail.com>
2020-01-14 12:47:03 +0800

    增加动态加载

---
+++ scripts/window-onload.js
@@ -0,0 +1,50 @@
+function loadHeadbar() {
+    let headbar = document.createElement('div');
+    document.body.prepend(headbar);
+    headbar.outerHTML = 
+    `<div class="mdui-appbar mdui-appbar-fixed mdui-appbar-scroll-toolbar-hide mdui-color-white">
+        <div class="mdui-toolbar mdui-color-theme mdui-col-offset-md-1">
+            <a href="../index.html" class="mdui-typo-title"><img src="https://s2.ax1x.com/2019/12/22/QxY8MR.png" height="25px" style="margin-bottom: -5px"> 异常OI</a>
+            <div class="mdui-toolbar-spacer"></div>
+            <a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">search</i></a>
+        </div>
+        <div class="mdui-tab" mdui-tab>
+            <a href="#" class="mdui-ripple mdui-ripple-white mdui-tab-active mdui-col-offset-md-1" onclick="window.location.href='../index.html'">首页</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">基础算法</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">动态规划</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">数据结构</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">数学</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">图论</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">计算几何</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">字符串</a>
+            <a href="#" class="mdui-ripple mdui-ripple-white">杂项</a>
+        </div>
+    </div>`;
+}
+
+function loadFootbar() {
+    let footbar = document.createElement('div');
+    document.getElementById('main').after(footbar);
+    footbar.outerHTML = 
+    `<div id="footbar">
+        <div id="left">
+            <img src="https://s2.ax1x.com/2020/01/13/lHmkJx.png" id="logo">
+            <div id="intro">异常OI</div>
+        </div>
+        <div id="right">
+            <a href="../intro/about.html">关于本站</a> | <a href="../intro/faq.html">使用帮助</a><br>
+            <a href="../intro/friends.html">友情链接</a> | <a href="../intro/thanks.html">特别感谢</a><br>
+            2020 , © 异常OI Developed by csp-yc Dev Team
+        </div>
+    </div>`;
+}
+
+function loadAll() {
+    loadHeadbar();
+    loadFootbar();
+    mdui.mutation();
+}
+
+(function() {
+    window.addEventListener('load', loadAll, false);
+})();
